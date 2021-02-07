a1fdbd033f24f18a5490db7f0977e346ee304141
Artyom Kombarov <artyom.kombaroff@gmail.com>
2021-02-07 22:41:07 +0300

    clean scrollAnimate.js

--- src/js/modules/scrollAnimate.js
+++ src/js/modules/scrollAnimate.js
@@ -1,11 +1,11 @@
 export default function scrollAnimate() {
-  const animItems = document.querySelectorAll('[data-anim]');
+  const animItems = document.querySelectorAll('[data-anim]')
 
   if (animItems.length > 0) {
     window.addEventListener('scroll', anim)
 
     function anim() {
-      const animStart = 4;
+      const animStart = 4
 
       animItems.forEach(item => {
         const
@@ -13,36 +13,36 @@ export default function scrollAnimate() {
           animRepeat = item.dataset.animRepeat,
           animDelay = item.dataset.animDelay,
           itemHeight = item.offsetHeight,
-          itemOffset = calcOffset(item);
+          itemOffset = calcOffset(item)
 
-        let animStartPoint;
+        let animStartPoint
         if (itemHeight > window.innerHeight) {
-          animStartPoint = window.innerHeight - window.innerHeight / animStart;
+          animStartPoint = window.innerHeight - window.innerHeight / animStart
         } else {
-          animStartPoint = window.innerHeight - itemHeight / animStart;
+          animStartPoint = window.innerHeight - itemHeight / animStart
         }
 
         if ((pageYOffset > itemOffset.top - animStartPoint) && pageYOffset < (itemOffset.top + itemHeight)) {
-          item.classList.add(animClass);
-          item.style.animationDelay = `${animDelay}`;
+          item.classList.add(animClass)
+          item.style.animationDelay = `${animDelay}`
         } else {
           if (animRepeat !== 'once') {
-            item.classList.remove(animClass);
-            item.style.animationDelay = `0s`;
+            item.classList.remove(animClass)
+            item.style.animationDelay = `0s`
           }
         }
       })
     }
 
     function calcOffset(elem) {
-      const coords = elem.getBoundingClientRect();
-      const scrollY = pageYOffset || document.documentElement.scrollTop;
-      const scrollX = pageXOffset || document.documentElement.scrollWidth;
-      return {top: coords.top + scrollY, left: coords.left + scrollX};
+      const coords = elem.getBoundingClientRect()
+      const scrollY = pageYOffset || document.documentElement.scrollTop
+      const scrollX = pageXOffset || document.documentElement.scrollWidth
+      return { top: coords.top + scrollY, left: coords.left + scrollX }
     }
 
     setTimeout(() => {
-      anim();
-    }, 500);
+      anim()
+    }, 500)
   }
 }
